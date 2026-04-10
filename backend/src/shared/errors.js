3282fd0db5739eaf0b698b56af3238137d877c7a
brillianttonsa <abdullatifmnyamis@gmail.com>
2026-04-10 07:56:17 +0300

    refractor: change codebase of erro file from js to ts

--- backend/src/shared/errors.js
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-    
-    constructor(message,statusCode = 400, code = "BAD_REQUEST") {
-        super(message);
-        this.statusCode = statusCode;
-        this.code = code;
-    }
-}
