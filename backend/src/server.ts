51eef8cb6a3429002471df563f4df360010eb2c8
brillianttonsa <abdullatifmnyamis@gmail.com>
2026-04-23 11:51:50 +0300

    backend initiate for production

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./app";
-import { env } from "./config/env";
-
-app.listen(env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`LifeHub backend running on port ${env.PORT}`);
-});
-
-
