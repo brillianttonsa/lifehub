b2fbd708da942b46e76ae1968715ab5efc7d4745
brillianttonsa <abdullatifmnyamis@gmaill.com>
2026-07-24 07:31:32 +0300

    add website page merged with its components

---
+++ frontend/src/pages/website/Website.tsx
@@ -0,0 +1,16 @@
+import { Hero, About, HowItsWorks, Modules, VisionCTA, Footer } from '../../components/website';
+
+
+export default function Website() {
+  return (
+    <>
+        <Hero />
+        <About />
+        <HowItsWorks />
+        <VisionCTA />
+        <Modules />
+        <Footer />
+    
+    </>
+  );
+}
